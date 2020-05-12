cfc9d13b905e03051accfb92cfff1f011022192b
devrangarajan <rangad@rpi.edu>
2020-05-12 09:56:36 -0700

    Fixed some dependencies and styling for blog

--- rd/src/components/textbox/textbox.css.js
+++ rd/src/components/textbox/textbox.css.js
@@ -2,7 +2,10 @@ import styled from 'styled-components';
 import { emphasis } from 'constants/theme';
 
 export const Container = styled.div`
-  text-align: justify;
-  padding: 0 25%;
+  text-align: left;
+  line-height: 1.2em;
+  padding: 0 29%;
   color: ${emphasis};
+  font-size: 24px;
+  font-family: georgia, "Times New Roman", serif;
 `;
